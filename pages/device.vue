b2c7f48f6827da0f6540b5215ebe6faa30589c11
Gökhan <gn.ozdemir@gmail.com>
2022-05-07 04:49:44 +0300

    get apis refactor
    under Chrome Web Sec Disabled mode

--- pages/device.vue
+++ pages/device.vue
@@ -1,30 +1,29 @@
 <script setup>
 import { server } from "process";
 
-// let devices = devicesState();
 const config = useRuntimeConfig();
 // const { data } = await useAsyncData("count", () => $fetch("/api/count"));
-const { data: organization } = await useFetch(
-  "https://console.helium.com/api/v1/organization",
-  {
-    headers: { key: config.heliumKey },
-    async onResponse({ request, response, options }) {
-      // Log response
-      // console.log("[fetch response]", request, response.status, response.body);
-    },
-  }
-);
+// const { data: organization } = await useFetch(
+//   "https://console.helium.com/api/v1/organization",
+//   {
+//     headers: { key: config.heliumKey },
+//     async onResponse({ request, response, options }) {
+//       // Log response
+//       // console.log("[fetch response]", request, response.status, response.body);
+//     },
+//   }
+// );
 
-const { data: devices } = await useFetch(
-  "https://console.helium.com/api/v1/devices",
-  {
-    headers: { key: config.heliumKey },
-    async onResponse({ request, response, options }) {
-      // Log response
-      // console.log("[fetch response]", request, response.status, response.body);
-    },
-  }
-);
+// const { data: devices } = await useFetch(
+//   "https://console.helium.com/api/v1/devices",
+//   {
+//     headers: { key: config.heliumKey },
+//     async onResponse({ request, response, options }) {
+//       // Log response
+//       // console.log("[fetch response]", request, response.status, response.body);
+//     },
+//   }
+// );
 
 // TODO: Merge the 2 requests above together: https://v3.nuxtjs.org/guide/features/data-fetching/ end of the page
 
@@ -97,6 +96,35 @@ const submitDevice = async (config) => {
   console.log("olalaaa");
 };
 
+let heliumState = ref({
+  organization: null,
+  devices: null,
+});
+
+const heliumOrgDetails = async () => {
+  heliumState.value.organization = await $fetch(
+    "https://console.helium.com/api/v1/organization",
+    {
+      method: "GET",
+      headers: {
+        key: config.heliumKey,
+      },
+    }
+  ).catch((error) => error.data);
+};
+
+const heliumDevices = async () => {
+  heliumState.value.devices = await $fetch(
+    "https://console.helium.com/api/v1/devices",
+    {
+      method: "GET",
+      headers: {
+        key: config.heliumKey,
+      },
+    }
+  ).catch((error) => error.data);
+};
+
 const mountainData = ref({
   title: "titt",
   description: "desc",
@@ -107,47 +135,48 @@ const mountaionDetailsGet = async () => {
     "https://api.nuxtjs.dev/mountains/mount-everest",
     {
       pick: ["title", "description"],
+      async onResponse({ request, response, options }) {
+        // Log response
+        console.log("[fetch response]", request, response.status);
+      },
     }
   );
 };
 </script>
 <template>
-  <!-- <div>Current Balance {{ data["dc_balance"] }}</div>
-  <div>Current Helium Name {{ data["name"] }}</div> -->
-  <div>Devices {{ organization }}</div>
-
   <div>
-    <!-- <div>
-      <button @click="loadMyTodo">Load my todo</button>
-      <div v-text="title"></div>
-    </div> -->
+    <div>
+      <div>Org {{ heliumState }}</div>
 
-    test
-    <form @submit.prevent="handleSubmit">
-      <input
-        v-model="form.name"
-        type="text"
-        name="name"
-        placeholder="Enter name"
-      />
-      <input
-        v-model="form.email"
-        type="email"
-        name="email"
-        placeholder="Enter email"
-      />
-      <textarea
-        v-model="form.message"
-        placeholder="Enter message"
-        name="message"
-      ></textarea>
-      <button type="submit" @click="submitDevice">Send</button>
-    </form>
-  </div>
-  <v-btn to="/"> Back </v-btn>
-  <v-btn @click="mountaionDetailsGet()"> mountaionDetailsGet </v-btn>
-  olala
-  <!-- {{ deviceNew }}
+      <div>
+        test
+        <form @submit.prevent="handleSubmit">
+          <input
+            v-model="form.name"
+            type="text"
+            name="name"
+            placeholder="Enter name"
+          />
+          <input
+            v-model="form.email"
+            type="email"
+            name="email"
+            placeholder="Enter email"
+          />
+          <textarea
+            v-model="form.message"
+            placeholder="Enter message"
+            name="message"
+          ></textarea>
+          <button type="submit" @click="submitDevice">Send</button>
+        </form>
+      </div>
+      <v-btn to="/"> Back </v-btn>
+      <v-btn @click="mountaionDetailsGet()"> mountaionDetailsGet </v-btn>
+      <v-btn @click="heliumOrgDetails()"> heliumOrgDetails </v-btn>
+      <v-btn @click="heliumDevices()"> heliumDevices </v-btn>
+      olala
+      <!-- {{ deviceNew }}
   olala
   <v-card v-if="deviceNew" class="mx-auto" max-width="344">
     <v-card-header>
@@ -165,29 +194,33 @@ const mountaionDetailsGet = async () => {
     <v-card-actions> </v-card-actions>
   </v-card>
   olala -->
-  <div style="background: lightblue" v-text="mountainData"></div>
 
-  {{ mountainData.title }}
-  {{ mountainData.description }}
-  <v-card
-    v-for="device in devices"
-    key="device.app_eui"
-    class="mx-auto"
-    max-width="344"
-  >
-    <v-card-header>
+      <div style="background: lightblue" v-text="mountainData"></div>
+
+      {{ mountainData.title }}
+      {{ mountainData.description }}
       <div>
-        <div class="text-overline mb-1">Device</div>
-        <div class="text-h6 mb-1">
-          <strong>Active: {{ device.active }}</strong>
-        </div>
-        <div class="text-caption">
-          {{ device.app_key }}
-          <span>{{ device }}</span>
-        </div>
+        <v-card
+          v-for="device in heliumState.devices"
+          key="device.app_eui"
+          class="mx-auto"
+          max-width="344"
+        >
+          <v-card-header>
+            <div>
+              <div class="text-overline mb-1">Device</div>
+              <div class="text-h6 mb-1">
+                <strong>Active: {{ device.active }}</strong>
+              </div>
+              <div class="text-caption">
+                {{ device.app_key }}
+                <span>{{ device }}</span>
+              </div>
+            </div>
+          </v-card-header>
+          <v-card-actions> </v-card-actions>
+        </v-card>
       </div>
-    </v-card-header>
-    <v-card-actions> </v-card-actions>
-  </v-card>
-  <!-- <div>Devices {{ mountain.title }}</div> -->
+    </div>
+  </div>
 </template>
